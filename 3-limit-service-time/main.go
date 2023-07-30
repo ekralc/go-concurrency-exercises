8697d30ed0e1d9b849617e7f79e7f62a32421a49
ekralc <35628223+ekralc@users.noreply.github.com>
2023-07-30 23:48:09 +0100

    Add solution 3

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,19 +10,53 @@
 
 package main
 
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
+	sync.Mutex
 }
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	ch := make(chan bool)
+	go func() {
+		process()
+		ch <- true
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		if u.IsPremium {
+			ch <- true
+			return
+		}
+		for {
+			<-ticker.C
+
+			atomic.AddInt64(&u.TimeUsed, 1)
+			fmt.Printf("User %v has accumulated %v seconds\n", u.ID, u.TimeUsed)
+
+			if atomic.LoadInt64(&u.TimeUsed) >= 10 {
+				ch <- false
+				break
+			}
+		}
+	}()
+
+	res := <-ch
+
+	return res
 }
 
 func main() {
